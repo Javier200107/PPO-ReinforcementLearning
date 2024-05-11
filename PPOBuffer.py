a6f9e31f60da981e1cd6551b29b7d31e78abaeb3
Javier200107 <xaviergonzalezbejar@gmail.com>
2024-05-11 12:58:33 +0200

    Buffer

---
+++ PPOBuffer.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+class PPOBuffer:
+
+    def __init__(self, batch_size) -> None:
+        self.batch_size = batch_size
+        self.states = []
+        self.actions = []
+        self.log_probs = []
+        self.values = []
+        self.rewards = []
+        self.dones = []
+
+    def generate_batches(self):
+        n_states = len(self.states)
+        batch_start = np.arange(0, n_states, self.batch_size)
+        indices = np.arange(n_states, dtype=np.int64)
+        np.random.shuffle(indices)
+        batches = [indices[i:i+self.batch_size] for i in batch_start]
+
+        return np.array(self.states), np.array(self.actions), np.array(self.log_probs), \
+                np.array(self.values), np.array(self.rewards), np.array(self.dones), batches
+
+    def store(self, state, action, log_prob, value, reward, done):
+        self.states.append(state)
+        self.actions.append(action)
+        self.log_probs.append(log_prob)
+        self.values.append(value)
+        self.rewards.append(reward)
+        self.dones.append(done)
+    
+    def clear(self):
+        self.states = []
+        self.actions = []
+        self.log_probs = []
+        self.values = []
+        self.rewards = []
+        self.dones = []
+        
