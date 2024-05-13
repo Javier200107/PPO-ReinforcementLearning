fd104290ded38cea684b7a736be6f4ad96411d2a
Javier200107 <xaviergonzalezbejar@gmail.com>
2024-05-13 12:39:41 +0200

    Update agent

--- PPOAgent.py
+++ PPOAgent.py
@@ -1,47 +1,27 @@
 import tensorflow as tf
 from tensorflow.keras.optimizers import Adam
 import keras
-from PPONetworks import ActorNetwork, CriticNetwork
+from PPONetworksDiscrete import ActorNetworkDiscrete, CriticNetworkDiscrete
+from PPONetworksContinuous import ActorNetworkContinuous, CriticNetworkContinuous
 import tensorflow_probability as tfp
-from PPOBuffer import PPOBuffer
+from PPOBufferDiscrete import PPOBufferDiscrete
+from PPOBufferContinuous import PPOBufferContinuous
 import numpy as np
 
 class PPOAgent:
     """Proximal Policy Optimization Agent using TensorFlow 2.0.0 and Keras."""
 
     def __init__(self, n_actions, input_dims, checkpoint_directory, gamma=0.99, alpha=0.0003, gae_lambda=0.95,
-                policy_clip=0.2, batch_size=64, n_epochs=10) -> None:
+                policy_clip=0.2, n_epochs=10) -> None:
         self.n_actions = n_actions
         self.input_dims = input_dims
         self.gamma = gamma
         self.alpha = alpha
         self.gae_lambda = gae_lambda
         self.policy_clip = policy_clip
-        self.batch_size = batch_size
         self.n_epochs = n_epochs
         self.checkpoint_directory = checkpoint_directory
-        
-        self.actor = ActorNetwork(n_actions)
-        self.critic = CriticNetwork()
-        self.actor.compile(optimizer=Adam(learning_rate=alpha))
-        self.critic.compile(optimizer=Adam(learning_rate=alpha))
 
-        self.buffer = PPOBuffer(batch_size)
-
-    def choose_action(self, observation):
-        state = tf.convert_to_tensor([observation])
-        probabilities = self.actor(state)
-        action_probs = tfp.distributions.Categorical(probs=probabilities)
-        action = action_probs.sample() # Sample an action from the distribution
-        log_probs = action_probs.log_prob(action) # Calculate the log probability of the action
-        value = self.critic(state) # Calculate the value of the state
-
-        action = action.numpy()[0]
-        value = value.numpy()[0]
-        log_probs = log_probs.numpy()[0]
-
-        return action, value, log_probs
-    
     def store_transition(self, state, action, log_prob, value, reward, done):
         self.buffer.store(state, action, log_prob, value, reward, done)
     
@@ -70,21 +50,7 @@ class PPOAgent:
                     batch_actions = tf.gather(actions, batch_indices)
                     batch_log_probs_old = tf.gather(log_probs_old, batch_indices)
 
-                    probs = self.actor(batch_states)
-                    new_log_probs = tfp.distributions.Categorical(probs=probs).log_prob(batch_actions)
-
-                    critic_value = tf.squeeze(self.critic(batch_states), 1)
-                    prob_rato = tf.math.exp(new_log_probs - batch_log_probs_old )
-
-                    weighted_probs = prob_rato * advantage[batch]
-                    clipped_probs = tf.clip_by_value(prob_rato, 1 - self.policy_clip, 1 + self.policy_clip)
-                    weighted_clipped_probs = clipped_probs * advantage[batch]
-
-                    actor_loss = -tf.reduce_mean(tf.math.minimum(weighted_probs, weighted_clipped_probs))
-                    actor_loss = tf.reduce_mean(actor_loss)
-
-                    returns = advantage[batch] + values[batch]
-                    critic_loss = keras.losses.MSE(critic_value, returns)
+                    actor_loss, critic_loss = self.update_networks(batch_states, batch_actions, batch_log_probs_old, advantage, batch, values)
                 
                 actor_params = self.actor.trainable_variables
                 critic_params = self.critic.trainable_variables
@@ -99,10 +65,110 @@ class PPOAgent:
                 
     def save_models(self):
         print("... saving models ...")
-        self.actor.save(self.checkpoint_directory + "/actor") # Save the whole actor model
-        self.critic.save(self.checkpoint_directory + "/critic") # Save the whole critic model
+        self.actor.save(self.checkpoint_directory + self.env_type + "/" + self.env_name + "/actor") # Save the whole actor model
+        self.critic.save(self.checkpoint_directory + self.env_type + "/" + self.env_name + "/critic") # Save the whole critic model
 
     def load_models(self):
         print("... loading models ...")
-        self.actor = keras.models.load_model(self.checkpoint_directory + "/actor")
-        self.critic = keras.models.load_model(self.checkpoint_directory + "/critic")
+        self.actor = keras.models.load_model(self.checkpoint_directory + "/" + self.env_type + self.env_name + "/actor")
+        self.critic = keras.models.load_model(self.checkpoint_directory + "/" + self.env_type + self.env_name + "/critic")
+
+class PPOAgentDiscrete(PPOAgent):
+    
+    def __init__(self, n_actions, input_dims, checkpoint_directory, env_name, gamma=0.99, alpha=0.0003, gae_lambda=0.95,
+                policy_clip=0.2, batch_size=64, n_epochs=10):
+        super(PPOAgentDiscrete, self).__init__(n_actions, input_dims, checkpoint_directory, gamma, alpha, gae_lambda,
+                policy_clip, n_epochs)
+        self.env_type = 'discrete'
+        self.env_name = env_name
+
+        self.actor = ActorNetworkDiscrete(n_actions=n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+        self.critic = CriticNetworkDiscrete(input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+
+        self.actor.compile(optimizer=Adam(learning_rate=alpha))
+        self.critic.compile(optimizer=Adam(learning_rate=alpha))
+
+        self.buffer = PPOBufferDiscrete(batch_size=batch_size)
+
+    def choose_action(self, observation):
+        state = tf.convert_to_tensor([observation])
+        probabilities = self.actor(state)
+        action_probs = tfp.distributions.Categorical(probs=probabilities)
+        action = action_probs.sample() # Sample an action from the distribution
+        log_probs = action_probs.log_prob(action) # Calculate the log probability of the action
+        value = self.critic(state) # Calculate the value of the state
+
+        action = action.numpy()[0]
+        value = value.numpy()[0]
+        log_probs = log_probs.numpy()[0]
+
+        return action, value, log_probs
+
+    def update_networks(self, batch_states, batch_actions, batch_log_probs_old, advantage, batch, values):
+        probs = self.actor(batch_states)
+        new_log_probs = tfp.distributions.Categorical(probs=probs).log_prob(batch_actions)
+
+        critic_value = tf.squeeze(self.critic(batch_states), 1)
+        prob_rato = tf.math.exp(new_log_probs - batch_log_probs_old)
+
+        weighted_probs = prob_rato * advantage[batch]
+        clipped_probs = tf.clip_by_value(prob_rato, 1 - self.policy_clip, 1 + self.policy_clip)
+        weighted_clipped_probs = clipped_probs * advantage[batch]
+
+        actor_loss = -tf.reduce_mean(tf.math.minimum(weighted_probs, weighted_clipped_probs))
+        actor_loss = tf.reduce_mean(actor_loss)
+
+        returns = advantage[batch] + values[batch]
+        critic_loss = keras.losses.MSE(critic_value, returns)
+
+        return actor_loss, critic_loss
+
+class PPOAgentContinuous(PPOAgent):
+    
+    def __init__(self, n_actions, input_dims, checkpoint_directory, env_name, gamma=0.99, alpha=0.0003, gae_lambda=0.95,
+                policy_clip=0.2, batch_size=64, n_epochs=10):
+        super(PPOAgentContinuous, self).__init__(n_actions, input_dims, checkpoint_directory, gamma, alpha, gae_lambda,
+                policy_clip, n_epochs)
+        self.env_type = 'continuous'
+        self.env_name = env_name
+
+        self.actor = ActorNetworkContinuous(n_actions=n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+        self.critic = CriticNetworkContinuous(input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+
+        self.actor.compile(optimizer=Adam(learning_rate=alpha))
+        self.critic.compile(optimizer=Adam(learning_rate=alpha))
+
+        self.buffer = PPOBufferContinuous(batch_size=batch_size)
+
+    def choose_action(self, observation):
+        state = tf.convert_to_tensor([observation], dtype=tf.float32)
+        mu, sigma = self.actor(state)
+        dist = tfp.distributions.Normal(mu, sigma)
+        action = dist.sample()  # Sample an action from the normal distribution
+        log_prob = dist.log_prob(action)
+        value = self.critic(state)
+
+        return action.numpy()[0], value.numpy()[0], log_prob.numpy()[0]
+    
+    def update_networks(self, batch_states, batch_actions, batch_log_probs_old, advantage, batch, values):
+        mu, sigma = self.actor(batch_states)  # Obtain mean and standard deviation for actions
+        dist = tfp.distributions.Normal(mu, sigma)
+        new_log_probs = dist.log_prob(batch_actions)
+
+        critic_value = tf.squeeze(self.critic(batch_states), 1)  # Get value estimate from critic
+        prob_ratio = tf.exp(new_log_probs - batch_log_probs_old)  # Calculate the probability ratio
+
+        # Apply the PPO clipped objective function
+        advantage_batch_expanded = tf.expand_dims(advantage[batch], axis=-1)
+        weighted_probs = prob_ratio * advantage_batch_expanded
+        clipped_probs = tf.clip_by_value(prob_ratio, 1 - self.policy_clip, 1 + self.policy_clip)
+        weighted_clipped_probs = clipped_probs * advantage_batch_expanded
+
+        # Actor loss calculation using the minimum of unclipped and clipped values
+        actor_loss = -tf.reduce_mean(tf.minimum(weighted_probs, weighted_clipped_probs))
+
+        # Calculate the returns and the critic loss
+        returns = advantage[batch] + values[batch]  # returns calculation might need adjustment based on your setup
+        critic_loss = keras.losses.MSE(critic_value, returns)
+
+        return actor_loss, critic_loss
