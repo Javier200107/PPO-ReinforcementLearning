e566a3befdfafdcacf54df598e855f359f04fa69
Javier200107 <xaviergonzalezbejar@gmail.com>
2024-05-16 12:21:12 +0200

    Unify buffers

--- PPOBuffer.py
+++ PPOBuffer.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-class PPOBufferDiscrete:
+class PPOBuffer:
+    """A class to store the experiences of the agent in the environment."""
 
     def __init__(self, batch_size) -> None:
         self.batch_size = batch_size
@@ -12,6 +13,7 @@ class PPOBufferDiscrete:
         self.dones = []
 
     def generate_batches(self):
+        """Generate batches of experiences."""
         n_states = len(self.states)
         batch_start = np.arange(0, n_states, self.batch_size)
         indices = np.arange(n_states, dtype=np.int64)
@@ -22,6 +24,7 @@ class PPOBufferDiscrete:
                 np.array(self.values), np.array(self.rewards), np.array(self.dones), batches
 
     def store(self, state, action, log_prob, value, reward, done):
+        """Store experiences in the buffer."""
         self.states.append(state)
         self.actions.append(action)
         self.log_probs.append(log_prob)
@@ -30,6 +33,7 @@ class PPOBufferDiscrete:
         self.dones.append(done)
     
     def clear(self):
+        """Clear the buffer."""
         self.states = []
         self.actions = []
         self.log_probs = []
